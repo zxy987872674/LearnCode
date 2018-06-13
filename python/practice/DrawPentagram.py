08af283be005fabf9b10e5c267e0f4b371ebe227
zhangxy <zhangxiangyu@xunlei.com>
2018-06-13 16:01:01 +0800

    practice

---
+++ python/practice/DrawPentagram.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+#coding=utf8
+
+#****************************************************
+# Author: 
+# Created: 2018-06-13 15:41
+# Last modified: 2018-06-13 15:41
+# Filename: DrawPentagram.py
+# Description: 
+#****************************************************
+
+import turtle
+turtle.color('red','red')
+turtle.begin_fill()
+for i in range(5):
+    fd(200)
+    rt(144)
+end_fill()
+done()
